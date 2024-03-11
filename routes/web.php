3bede4fa76251b8d3d26e3d3b5e551bd8908ff24
zakaria-houam <zakariahouamzakaria@gmail.com>
2024-03-11 13:00:32 +0100

    api route

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Route::group(['prefix' => 'api'], function () {
+    // Include Laravel's default authentication routes here
+    Auth::routes();
+});
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
